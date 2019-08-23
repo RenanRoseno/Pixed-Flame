0293ab4f55487f285242bbb15c1abbc8235ced89
Renan Roseno <rosenorenan@gmail.com>
2019-08-23 15:04:44 -0300

    Adicionando criação de pixels

--- fire.js
+++ fire.js
@@ -1,12 +1,13 @@
 const flamePixed = []
-const flameWidth = 3
-const flameHeight = 3
+const flameWidth = 20
+const flameHeight = 11      
 
 
 
 function start(){
     createStructure()
     console.log(flamePixed)
+    renderFlame()
 }
 
 function calculateFlamePropagation(){
@@ -18,11 +19,31 @@ function createStructure(){
 
     for(let i = 0; i < numberOfPixels;i++){
         flamePixed[i] = 0
-        console
     }
 }
 
-function renderFire(){
+function renderFlame(){
+    let html = '<table cellpadding=0 cellspacing=0 align=center>'
 
+    for(let row = 0; row < flameHeight; row++){
+        html += '<tr>'
+
+        for(let column = 0; column < flameWidth;column++){
+            const pixelIndex = column + (flameWidth * row)
+            const flameIntesity = flamePixed[pixelIndex]
+
+            html += '<td>'
+            html += `<div class= "pixel-index">${pixelIndex}</div>`
+            html += flameIntesity
+            html += '</td>'
+        }
+
+        html += '</tr>'
+    }
+    html += '</table>'
+
+    document.querySelector('#Flame').innerHTML = html
 }
+
+
 start()
